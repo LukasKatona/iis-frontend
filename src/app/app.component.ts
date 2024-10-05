b51550920201a9544aa5a28015606e7e289c0a87
Lukas Katona <katona.lukasek@gmail.com>
2024-10-05 14:06:05 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'efarm';
+
+  public testEndpoint() {
+    const baseUri = "https://closed-sapphire-lukokat-71c0d315.koyeb.app/";
+
+    // Test GET request
+    fetch(baseUri + "items/0")
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
 }
