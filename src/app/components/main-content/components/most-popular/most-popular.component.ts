63442965fe16f1da5bec430f43009f79b6b6d171
Lukas Katona <katona.lukasek@gmail.com>
2024-11-20 17:23:43 +0100

    feat: most popular

--- src/app/components/main-content/components/most-popular/most-popular.component.ts
+++ src/app/components/main-content/components/most-popular/most-popular.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Product } from '../../../../../models/product.interface';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../../environments/environment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-most-popular',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './most-popular.component.html',
-  styleUrl: './most-popular.component.scss'
+  styleUrl: './most-popular.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class MostPopularComponent {
+  public products: Product[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getMostPopularProducts();
+  }
+
+  private getMostPopularProducts(): void {
+    let url = environment.baseUri + '/products/most-popular';
+
+    this.http.get<Product[]>(url)
+      .subscribe((data: Product[]) => {
+        this.products = data;
+      });
+  }
 }
