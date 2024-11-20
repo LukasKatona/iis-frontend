4edf2d285a5655fd73294694d1082960781f6b4a
Lukas Katona <katona.lukasek@gmail.com>
2024-11-20 16:27:29 +0100

    feat: farmer product card style

--- src/app/components/main-content/components/farmer-products/components/farmer-card-product/farmer-product-card/farmer-product-card.component.ts
+++ src/app/components/main-content/components/farmer-products/components/farmer-card-product/farmer-product-card/farmer-product-card.component.ts
@@ -2,11 +2,12 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, Input, Output, EventEmitter } from '
 import { Product } from '../../../../../../../../models/product.interface';
 import { environment } from '../../../../../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-farmer-product-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './farmer-product-card.component.html',
   styleUrls: ['./farmer-product-card.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
