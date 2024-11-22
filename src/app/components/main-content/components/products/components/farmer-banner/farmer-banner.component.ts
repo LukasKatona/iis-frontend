6b54997f12ab09dec4a7b1f99121158c7e0296f9
L-Sinister <klarkakejdova@gmail.com>
2024-11-22 16:08:01 +0100

    fix: farmer banner info

--- src/app/components/main-content/components/products/components/farmer-banner/farmer-banner.component.ts
+++ src/app/components/main-content/components/products/components/farmer-banner/farmer-banner.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Farmer } from '../../../../../../../models/farmer.interface';
+import { User } from '../../../../../../../models/user.interface';
+import { environment } from '../../../../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-farmer-banner',
@@ -13,4 +16,18 @@ import { Farmer } from '../../../../../../../models/farmer.interface';
 export class FarmerBannerComponent {
   @Input()
   farmer!: Farmer;
+  public user: User | undefined
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getFarmerInfo(this.farmer.userId);
+  }
+
+  public getFarmerInfo(userId: number): void {
+    this.http.get<User>(`${environment.baseUri}/users/${userId}`).subscribe((data: User) => {
+      this.user = data;
+    });
+  }
+
 }
