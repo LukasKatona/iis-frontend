e3b52bab2e4255d2cf46ee3d9e75e441206c4197
Lukas Katona <katona.lukasek@gmail.com>
2024-11-24 09:38:09 +0100

    feat: hash routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptorInterceptor } from './interceptors/auth-interceptor.interceptor';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideAnimationsAsync(),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([authInterceptorInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptorInterceptor])),
   ]
 };
